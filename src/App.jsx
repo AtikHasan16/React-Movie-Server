4ded40aa3789180c6a20b54d9c8eaae28d3ef2ce
AtikHasan16 <akhasannaeem@gmail.com>
2025-09-21 14:14:53 +0600

    Setup the files and initial folders

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import MovieCard from "./Components/MovieCard";
+
+function App() {
+  return (
+    <>
+      <h1>Hello React</h1>
+      <MovieCard></MovieCard>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/MovieCard.jsx
@@ -0,0 +1,11 @@
+const MovieCard = () => {
+  return (
+    <>
+      <div>
+        <h1>Movie Card</h1>
+      </div>
+    </>
+  );
+};
+
+export default MovieCard;
